24f34db8e4b89ecaf1c37532ea2abfd30dd6fbc2
mannbajpai <mannbajpai.grs@gmail.com>
2023-06-12 02:30:10 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Navbar, Hero, About, Projects, Contact, Footer } from "./components"
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import About from "./About"
+import Contact from "./Contact"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import Navbar from "./Navbar"
+import Projects from "./Projects"
+
+export {
+    About,
+    Contact,
+    Footer,
+    Hero,
+    Navbar,
+    Projects
+}
